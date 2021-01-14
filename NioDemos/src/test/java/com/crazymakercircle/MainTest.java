624e7d628e7930a0353b88e8ef5f0afa5db66d12
gaocai <gaocai@vipkid.com.cn>
2021-01-14 14:31:29 +0800

    test

---
+++ NioDemos/src/test/java/com/crazymakercircle/MainTest.java
@@ -0,0 +1,11 @@
+package com.crazymakercircle;
+
+/**
+ * @author GaoCai  电商交易平台，财务结算组
+ * @date 2021/1/14   -   14:28
+ **/
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
